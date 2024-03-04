9e1689ae6c208dbdee4fa652ad8ade6e4bcb7c7c
Gabriela Lupita <gabriela@un-j3.kzn.21-school.ru>
2024-03-04 23:43:36 +0300

    ex01 wrong order

--- src/cmd/compareDB/compareDB.go
+++ src/cmd/compareDB/compareDB.go
@@ -6,73 +6,96 @@ import (
 	"day01/internal/DBReader"
 )
 
-// func checkTime(compareCakes *[]string, old *RecipeBook, new *RecipeBook) {
-// 	// for _, real_cake := range compareCakes  {
-
-// 	// }
-// 	for _, new_cake := range new.Recipes {
-//         for _, old_cake := range old.Recipes {
-// 			if new_cake.Name != old_cake.Name {
-// 				continue
-// 			}
-// 			if new_cake.Time != old_cake.Time {
-// 				fmt.Printf("CHANGED cooking time for cake %q - %q instead of %q\n", new_cake.Name, old_cake.Time, new_cake.Time)
-// 			}
-// 			checkIngredients(&new_cake.Ingredients, &old_cake.Ingredients)
-// 		}
-//     }
-// }
-
-// func checkIngredients(new *[]Item, old *[]Item) {
-// 	for _, new_item := range new {
-//         for _, old_item := range old {
-// 			if new_item.Name == old_item.Name {
-// 				// sameCake = append(sameCake, old_cake.Name)
-// 				continue
-// 			}
-// 			fmt.Printf("ADDED ingredient %q for cake %q\n", new_item.Name, new_cake.Name)
-// 		}
-//     }
-
-// 	for _, old_cake := range old.Recipes {
-//         for _, real_cake := range sameCake {
-// 			if old_cake.Name == real_cake {
-// 				continue
-// 			}
-// 			fmt.Printf("REMOVED cake %q\n", old_cake.Name)
-// 		}
-//     }
-// }
-
-// func checkCount() {
-	
-// }
-
 // func checkUnit() {
 	
 // }
 
-func checkCakes(old RecipeBook, new RecipeBook) []string {
-	var sameCake []string
+func checkCakes(old *DBReader.RecipeBook, new *DBReader.RecipeBook) {
+	same := false
 	for _, new_cake := range new.Recipes {
         for _, old_cake := range old.Recipes {
 			if new_cake.Name == old_cake.Name {
-				sameCake = append(sameCake, old_cake.Name)
+				checkTime(old_cake.Name, old_cake.Time, new_cake.Time)
+				checkIngredients(&old_cake, &new_cake)
+				same = true
 				break
 			}
 		}
-		fmt.Printf("ADDED cake %q\n", new_cake.Name)
+		if !same {
+			fmt.Printf("ADDED cake %q\n", new_cake.Name)
+		} else {
+			same = false
+		}
     }
 
 	for _, old_cake := range old.Recipes {
-        for _, real_cake := range sameCake {
-			if old_cake.Name == real_cake {
-				continue
+        for _, new_cake := range new.Recipes {
+			if old_cake.Name == new_cake.Name {
+				same = true
+				break
 			}
+		}
+		if !same {
 			fmt.Printf("REMOVED cake %q\n", old_cake.Name)
+		} else {
+			same = false
 		}
     }
-	return sameCake
+}
+
+func checkTime(cakeName string, oldTime string, newTime string) {
+	if oldTime != newTime {
+		fmt.Printf("CHANGED cooking time for cake %q - %q instead of %q\n", cakeName, newTime, oldTime)
+    }
+}
+
+func checkIngredients(old *DBReader.Cake, new *DBReader.Cake) {
+	same := false
+	for _, new_item := range new.Ingredients {
+        for _, old_item := range old.Ingredients {
+			if new_item.Name == old_item.Name {
+				checkUnit(old.Name, &old_item, &new_item)
+				same = true
+				break
+			}
+		}
+		if !same {
+			fmt.Printf("ADDED ingredient %q for cake %q\n", new_item.Name, new.Name)
+		} else {
+			same = false
+		}
+    }
+
+	for _, old_item := range old.Ingredients {
+        for _, new_item := range new.Ingredients {
+			if old_item.Name == new_item.Name {
+				same = true
+				break
+			}
+		}
+		if !same {
+			fmt.Printf("REMOVED ingredient %q for cake %q\n", old_item.Name, old.Name)
+		} else {
+			same = false
+		}
+    }
+}
+
+
+func checkUnit(cakeName string, old *DBReader.Item, new *DBReader.Item) {
+	if old.Unit != new.Unit {
+		switch {
+		case old.Unit == "":
+				fmt.Printf("ADDED unit %q for ingredient %q for cake  %q\n", new.Unit, new.Name, cakeName)
+		case new.Unit == "":
+				fmt.Printf("ADDED unit %q for ingredient %q for cake  %q\n", old.Unit, new.Name, cakeName)
+		default:
+			fmt.Printf("CHANGED unit for ingredient %q for cake  %q - %q instead of %q\n", new.Name, cakeName, new.Unit, old.Unit)
+		}
+    }
+	if old.Count != new.Count {
+		fmt.Printf("CHANGED unit count for ingredient %q for cake  %q - %q instead of %q\n", new.Name, cakeName, new.Count, old.Count)
+    }
 }
 
 func main() {
@@ -93,6 +116,5 @@ func main() {
 	reader = DBReader.New(newFile)
 	newRecipeBook := reader.Read()
 
-	compareCakes := checkCakes(oldRecipeBook, newRecipeBook)
-
+	checkCakes(&oldRecipeBook, &newRecipeBook)
 }
